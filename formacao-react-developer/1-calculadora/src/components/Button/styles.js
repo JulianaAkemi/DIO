803ddb4f8a885cca00c3b18d0cdd2e3540b16247
Juliana <julianaakkemi@gmail.com>
2025-08-06 13:40:33 -0300

    fix estilo botao apagar, add pacote styled-components e funcoes de dividir e multiplicar

--- formacao-react-developer/1-calculadora/src/components/Button/styles.js
+++ formacao-react-developer/1-calculadora/src/components/Button/styles.js
@@ -1,15 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-    padding: 20px;
-    border: 1px solid #CDCDCD;
-    background-color: #00AAF0;
-    color: #FFFFFF;
-    font-size: 24px;
-    font-weight: 700;
-    flex: 1;
-    
-    &:hover {
-        opacity: 0.6;
-    }
-`
+  padding: 20px;
+  border: 1px solid #cdcdcd;
+  background-color: #00aaf0;
+  color: #ffffff;
+  font-size: 24px;
+  font-weight: 700;
+  ${({ children }) => (children === "c" ? "width: 50%;" : "flex: 1;")};
+
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+`;
